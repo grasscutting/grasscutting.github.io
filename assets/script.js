436179aeb3c711339a56ed0e07ede978994ed3bb
DaBlincx <blincxyt@gmail.com>
2023-06-02 22:08:23 +0200

    d

--- assets/script.js
+++ assets/script.js
@@ -6,27 +6,32 @@ let text = 'Coming soon';
 let dots = '';
 
 function fadeInText() {
-  let interval = setInterval(function() {
-    if (text.length === 0) {
-      clearInterval(interval);
-      animateDots();
-    } else {
-      let letter = text.charAt(0);
-      textcontainer.innerHTML += letter;
-      text = text.slice(1);
-    }
-  }, 500);
+    let interval = setInterval(function () {
+        if (text.length === 0) {
+            clearInterval(interval);
+            animateDots();
+        } else {
+            let letter = text.charAt(0);
+            textcontainer.innerHTML += letter;
+            text = text.slice(1);
+        }
+    }, 500);
 }
 
 function animateDots() {
-  let dotInterval = setInterval(function() {
-    if (dots === '...') {
-      dots = '';
-    } else {
-      dots += '.';
-    }
-    textcontainer.innerHTML = 'Coming soon' + dots;
-  }, 500);
+    let dotInterval = setInterval(function () {
+        if (dots === '...') {
+            dots = '';
+        } else {
+            dots += '.';
+        }
+        // set margin of textcontainer to the width of the dots so that the text doesn't jump around
+        let prefixspace = '';
+        for (let i = 0; i < dots.length; i++) {
+            prefixspace += '&nbsp;';
+        }
+        textcontainer.innerHTML = prefixspace + 'Coming soon' + dots;
+    }, 500);
 }
 
 fadeInText();
